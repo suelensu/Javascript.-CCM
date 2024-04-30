6b29b214079f7d12f14cd48ae3e3867370f5392a
suelensu <126163733+suelensu@users.noreply.github.com>
2024-04-30 07:51:31 -0300

    data

--- main.js
+++ main.js
@@ -14,9 +14,9 @@ for(let i=0; i<botoes.length; i++){
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-04-01T00:00:00");
-const tempoObjetivo2 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-10-05T00:00:00");
-const tempoObjetivo4 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo2 = new Date("2025-01-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-09-17T00:00:00");
+const tempoObjetivo4 = new Date("2025-01-01T00:00:00");
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
 
